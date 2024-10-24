50ebfccd1d126265836d50673fbc4c5df1d95647
Yu Hao Shao <yuhaoshao@Yus-MacBook-Air.local>
2024-10-24 16:56:36 -0700

    basic landing page added

---
+++ src/lib/formatters.ts
@@ -0,0 +1,5 @@
+const compantNumberFormatter = new Intl.NumberFormat(undefined, { notation: 'compact'})
+
+export function formatCompactNumber(number:number){
+    return compantNumberFormatter.format(number)
+}
