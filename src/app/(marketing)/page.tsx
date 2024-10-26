0f19a4f097c7d2e6e64741f36c2f80acf66e6b0c
Yu Hao Shao <yuhaoshao@Yus-MacBook-Air.local>
2024-10-26 14:49:17 -0700

    footer section added

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -9,6 +9,7 @@ import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle }
 import { formatCompactNumber } from "@/lib/formatters";
 import { ReactNode } from "react";
 import { cn } from "@/lib/utils";
+import { BrandLogo } from "@/components/BrandLogo";
 
 export default function HomePage() {
   return (
@@ -78,6 +79,84 @@ export default function HomePage() {
           ))}
         </div>
       </section>
+
+     <footer className='container pt-16 pb-8 flex flex-col sm:flex-row gap-8 sm:gap-4 justify-between items-start'>
+       <Link href='/'>
+         <BrandLogo />
+       </Link>
+       <div className='flex flex-col sm:flex-row gap-40'>
+          <div className="flex flex-col gap-8">
+            <FooterLinkGroup 
+              title='help' 
+              links={[
+                { label:'PPP Discounts', href:'#' },
+                { label:'Holiday discounts', href:'#' },
+                { label:'Time based discounts', href:'#' },
+                { label:'Discount API', href:'#' },
+                { label:'Add banner to site', href:'#' },
+                { label:'Contact', href:'#' },
+              ]}
+            />
+            <FooterLinkGroup
+              title="Solutions"
+              links={[
+                { label: "Newsletter", href: "#" },
+                { label: "SaaS Business", href: "#" },
+                { label: "Online Courses", href: "#" },
+              ]}
+            />
+          </div>
+          <div className="flex flex-col gap-8">
+            <FooterLinkGroup
+              title="Features"
+              links={[{ label: "PPP Discounts", href: "#" }]}
+            />
+            <FooterLinkGroup
+              title="Tools"
+              links={[
+                { label: "Salary Converter", href: "#" },
+                { label: "Coupon Generator", href: "#" },
+                { label: "Stripe App", href: "#" },
+              ]}
+            />
+             <FooterLinkGroup
+              title="Company"
+              links={[
+                { label: "Affiliate", href: "#" },
+                { label: "Twitter", href: "#" },
+                { label: "Terms of Service", href: "#" },
+              ]}
+            />
+
+          </div>
+
+          <div className="flex flex-col gap-8">
+            <FooterLinkGroup
+              title="Integrations"
+              links={[
+                { label: "Lemon Squeezy", href: "#" },
+                { label: "Gumroad", href: "#" },
+                { label: "Stripe", href: "#" },
+                { label: "Chargebee", href: "#" },
+                { label: "Paddle", href: "#" },
+              ]}
+            />
+            <FooterLinkGroup
+              title="Tutorials"
+              links={[
+                { label: "Any Website", href: "#" },
+                { label: "Lemon Squeezy", href: "#" },
+                { label: "Gumroad", href: "#" },
+                { label: "Stripe", href: "#" },
+                { label: "Chargebee", href: "#" },
+                { label: "Paddle", href: "#" },
+              ]}
+            />
+          </div>
+       </div>
+       
+      
+     </footer>
     </>
   );
 }
@@ -128,4 +207,25 @@ function Feature({children, className}: {children:ReactNode, className?: string}
       <CheckIcon className='size-5 text-cyan-400 bg-cyan-100 rounded-full p-0.5 '/>
       <span>{children}</span>
     </div>
+}
+
+function FooterLinkGroup({ 
+  title, 
+  links,
+}: { 
+  title:string
+  links:{ label:string, href:string }[] 
+}){
+  return (<div className='flex flex-col gap-4'>
+    <h3 className="font-semibold">{title}</h3>
+    <ul className='flex flex-col gap-2 text-sm'>
+      {links.map(link=>(
+        <li key={link.href}>
+          <Link href={link.href}>{link.label}</Link>
+        </li>
+      ))}
+    </ul>
+    
+  </div>)
+
 }
